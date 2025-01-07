7c47fd77ad7ada6e02314b6a1da7efa8f6bb75c6
bje <baejungeun11@gmail.com>
2025-01-07 21:29:44 +0900

    Point.userId 에 unique 추가

--- src/main/java/kr/hhplus/be/server/domain/point/domain/Point.java
+++ src/main/java/kr/hhplus/be/server/domain/point/domain/Point.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.point.domain;
 
 import static kr.hhplus.be.server.common.exception.IllegalArgumentErrorCode.*;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Point extends BaseEntity {
     private long balance;
 
     @Setter
+    @Column(unique = true)
     private long userId;
 
     public long charge(long amount) {
