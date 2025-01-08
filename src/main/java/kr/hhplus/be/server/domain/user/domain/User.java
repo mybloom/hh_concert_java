0eb6da6dc885c2557de5d30e556a7b719640484e
bje <baejungeun11@gmail.com>
2025-01-09 07:01:08 +0900

    테스트코드를 위한 User 객체 생성용  AllArgsConstructor 작성

--- src/main/java/kr/hhplus/be/server/domain/user/domain/User.java
+++ src/main/java/kr/hhplus/be/server/domain/user/domain/User.java
@@ -5,8 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Table(name = "users")
 @Entity
