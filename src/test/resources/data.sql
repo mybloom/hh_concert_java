78a17c67a6acdfac47ac387b1f75c51a8b84f4d9
bje <baejungeun11@gmail.com>
2025-01-09 20:07:10 +0900

    test용 data.sql 토큰 관련 insert문 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -12,8 +12,23 @@ INSERT INTO users (name) VALUES
 ('User11'),
 ('User12'),
 ('User13'),
-('User14');
-
+('User14'),
+('User15'),
+('User16'),
+('User17'),
+('User18'),
+('User19'),
+('User20'),
+('User21'),
+('User22'),
+('User23'),
+('User24'),
+('User25'),
+('User26'),
+('User27'),
+('User28'),
+('User29'),
+('User30');
 
 INSERT INTO point (user_id, balance) VALUES
 (1, 0),
@@ -21,39 +36,46 @@ INSERT INTO point (user_id, balance) VALUES
 (3, 0);
 
 
-INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440000', 1, 'ACTIVE', '2025-01-08 10:00:00');
+INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at) VALUES
+('550e8400-e29b-41d4-a716-44665544000A', 11, 'INVALID', '2025-01-08 11:30:00'),
+('550e8400-e29b-41d4-a716-44665544000B', 12, 'INVALID', '2025-01-08 11:30:00'),
+('550e8400-e29b-41d4-a716-44665544000C', 13, 'INVALID', '2025-01-08 11:30:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440001', 2, 'ACTIVE', '2025-01-08 10:10:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440000', 1, 'ACTIVE', '2025-01-10 10:00:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440002', 3, 'ACTIVE', '2025-01-08 10:20:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440001', 2, 'ACTIVE', '2025-01-10 10:10:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440003', 4, 'ACTIVE', '2025-01-08 10:30:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440002', 3, 'ACTIVE', '2025-01-10 10:20:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440004', 5, 'ACTIVE', '2025-01-08 10:40:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440003', 4, 'ACTIVE', '2025-01-10 10:30:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440005', 6, 'ACTIVE', '2025-01-08 10:50:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440004', 5, 'ACTIVE', '2025-01-10 10:40:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440006', 7, 'ACTIVE', '2025-01-08 11:00:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440005', 6, 'ACTIVE', '2025-01-10 10:50:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440007', 8, 'ACTIVE', '2025-01-08 11:10:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440006', 7, 'ACTIVE', '2025-01-10 11:00:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440008', 9, 'ACTIVE', '2025-01-31 11:20:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440007', 8, 'ACTIVE', '2025-01-10 11:10:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440009', 10, 'ACTIVE', '2025-01-08 11:30:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440008', 9, 'ACTIVE', '2025-01-10 11:20:00');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440010', 11, 'INVALID', '2025-01-08 11:33:00');
+VALUES ('550e8400-e29b-41d4-a716-446655440009', 10, 'ACTIVE', '2025-01-10 11:30:00');
 
-INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at, wait_offset)
-VALUES ('550e8400-e29b-41d4-a716-446655440011', 12, 'WAIT', '2025-01-08 11:35:00', 1);
---
+INSERT INTO queue_token (token_uuid, user_id, status, wait_offset) VALUES
+('550e8400-e29b-41d4-a716-446655440021', 14, 'WAIT', 1),
+('550e8400-e29b-41d4-a716-446655440022', 15, 'WAIT', 2),
+('550e8400-e29b-41d4-a716-446655440023', 16, 'WAIT', 3),
+('550e8400-e29b-41d4-a716-446655440024', 17, 'WAIT', 4),
+('550e8400-e29b-41d4-a716-446655440025', 18, 'WAIT', 5);
+
+INSERT INTO queue_offset(last_active_offset) VALUES(13);
