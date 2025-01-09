03f707fa3e0cc972f75899f65885e7d12b2d73ae
bje <baejungeun11@gmail.com>
2025-01-10 02:47:09 +0900

    콘서트의 스케줄 샘플 데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -186,3 +186,45 @@ INSERT INTO queue_token (token_uuid, user_id, status, wait_offset) VALUES
 
 
 INSERT INTO queue_offset(last_active_offset) VALUES(30);
+
+
+
+INSERT INTO Concert (name) VALUES ('Rock Festival');
+INSERT INTO Concert (name) VALUES ('Jazz Night');
+INSERT INTO Concert (name) VALUES ('Pop Concert');
+
+INSERT INTO Schedule (concert_Id, schedule_date) VALUES
+(1, '2024-01-05'),
+(1, '2024-01-10'),
+(1, '2024-01-20'),
+(1, '2024-02-05'),
+(1, '2024-02-10'),
+(1, '2024-02-20'),
+(1, '2024-03-05'),
+(1, '2024-03-10'),
+(1, '2024-03-20'),
+(1, '2024-04-05'),
+(1, '2024-04-10'),
+(1, '2024-04-20'),
+(1, '2024-05-05'),
+(1, '2024-05-10'),
+(1, '2024-05-20'),
+(1, '2024-06-05'),
+(1, '2024-06-10'),
+(1, '2024-06-15'),
+(1, '2024-07-05'),
+(1, '2024-07-10'),
+(1, '2024-07-20'),
+(1, '2024-08-05'),
+(1, '2024-08-10'),
+(1, '2024-08-20'),
+(1, '2024-09-05'),
+(1, '2024-09-10'),
+(1, '2024-09-20'),
+(1, '2024-10-05'),
+(1, '2024-10-10'),
+(1, '2024-10-20');
+INSERT INTO Schedule (concert_Id, schedule_date) VALUES (2, '2024-06-15');
+INSERT INTO Schedule (concert_Id, schedule_date) VALUES (2, '2024-07-10');
+INSERT INTO Schedule (concert_Id, schedule_date) VALUES (2, '2024-08-20');
+
