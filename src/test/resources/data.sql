c6de938511cb981053f651e30409a91f21e8afca
bje <baejungeun11@gmail.com>
2025-01-10 04:09:35 +0900

    test data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -66,70 +66,9 @@ INSERT INTO point (user_id, balance) VALUES
 (2, 0),
 (3, 0);
 
-INSERT INTO users (name) VALUES
-('User1'),
-('User2'),
-('User3'),
-('User4'),
-('User5'),
-('User6'),
-('User7'),
-('User8'),
-('User9'),
-('User10'),
-('User11'),
-('User12'),
-('User13'),
-('User14'),
-('User15'),
-('User16'),
-('User17'),
-('User18'),
-('User19'),
-('User20'),
-('User21'),
-('User22'),
-('User23'),
-('User24'),
-('User25'),
-('User26'),
-('User27'),
-('User28'),
-('User29'),
-('User30'),
-('User31'),
-('User32'),
-('User33'),
-('User34'),
-('User35'),
-('User36'),
-('User37'),
-('User38'),
-('User39'),
-('User40'),
-('User41'),
-('User42'),
-('User43'),
-('User44'),
-('User45'),
-('User46'),
-('User47'),
-('User48'),
-('User49'),
-('User50'),
-('User51'),
-('User52'),
-('User53'),
-('User54'),
-('User55'),
-('User56'),
-('User57'),
-('User58'),
-('User59'),
-('User60');
 
 INSERT INTO queue_token (token_uuid, user_id, status, running_expired_at) VALUES
-('550e8400-e29b-41d4-a716-446655440001', 1, 'INVALID', '2025-01-08 11:30:00'),
+('550e8400-e29b-41d4-a716-446655440001', 1, 'ACTIVE', '2025-01-08 11:30:00'),
 ('550e8400-e29b-41d4-a716-446655440002', 2, 'ACTIVE', '2025-01-10 10:00:00'),
 ('550e8400-e29b-41d4-a716-446655440003', 3, 'INVALID', '2025-01-08 11:30:00'),
 ('550e8400-e29b-41d4-a716-446655440004', 4, 'ACTIVE', '2025-01-10 10:00:00'),
@@ -184,16 +123,14 @@ INSERT INTO queue_token (token_uuid, user_id, status, wait_offset) VALUES
 ('550e8400-e29b-41d4-a716-446655440043', 49, 'WAIT', 19),
 ('550e8400-e29b-41d4-a716-446655440044', 50, 'WAIT', 20);
 
-
 INSERT INTO queue_offset(last_active_offset) VALUES(30);
 
 
+INSERT INTO Schedule (concert_id, schedule_date) VALUES (1, '2024-06-15');
+INSERT INTO Schedule (concert_id, schedule_date) VALUES (2, '2024-07-10');
+INSERT INTO Schedule (concert_id, schedule_date) VALUES (3, '2024-08-20');
 
-INSERT INTO Concert (name) VALUES ('Rock Festival');
-INSERT INTO Concert (name) VALUES ('Jazz Night');
-INSERT INTO Concert (name) VALUES ('Pop Concert');
-
-INSERT INTO Schedule (concert_Id, schedule_date) VALUES
+INSERT INTO Schedule (concert_id, schedule_date) VALUES
 (1, '2024-01-05'),
 (1, '2024-01-10'),
 (1, '2024-01-20'),
@@ -224,7 +161,56 @@ INSERT INTO Schedule (concert_Id, schedule_date) VALUES
 (1, '2024-10-05'),
 (1, '2024-10-10'),
 (1, '2024-10-20');
-INSERT INTO Schedule (concert_Id, schedule_date) VALUES (2, '2024-06-15');
-INSERT INTO Schedule (concert_Id, schedule_date) VALUES (2, '2024-07-10');
-INSERT INTO Schedule (concert_Id, schedule_date) VALUES (2, '2024-08-20');
 
+INSERT INTO Seat (schedule_id, seat_no, price, status)
+VALUES
+(1, 1, 10000, 'AVAILABLE'),
+(1, 2, 10000, 'AVAILABLE'),
+(1, 3, 10000, 'AVAILABLE'),
+(1, 4, 10000, 'AVAILABLE'),
+(1, 5, 10000, 'AVAILABLE'),
+(1, 6, 10000, 'AVAILABLE'),
+(1, 7, 10000, 'AVAILABLE'),
+(1, 8, 10000, 'AVAILABLE'),
+(1, 9, 10000, 'AVAILABLE'),
+(1, 10, 10000, 'AVAILABLE'),
+(1, 11, 10000, 'AVAILABLE'),
+(1, 12, 10000, 'AVAILABLE'),
+(1, 13, 10000, 'AVAILABLE'),
+(1, 14, 10000, 'AVAILABLE'),
+(1, 15, 10000, 'AVAILABLE'),
+(1, 16, 10000, 'AVAILABLE'),
+(1, 17, 10000, 'AVAILABLE'),
+(1, 18, 10000, 'AVAILABLE'),
+(1, 19, 10000, 'AVAILABLE'),
+(1, 20, 10000, 'AVAILABLE'),
+(1, 21, 10000, 'AVAILABLE'),
+(1, 22, 10000, 'AVAILABLE'),
+(1, 23, 10000, 'AVAILABLE'),
+(1, 24, 10000, 'AVAILABLE'),
+(1, 25, 10000, 'AVAILABLE'),
+(1, 26, 10000, 'AVAILABLE'),
+(1, 27, 10000, 'AVAILABLE'),
+(1, 28, 10000, 'AVAILABLE'),
+(1, 29, 10000, 'AVAILABLE'),
+(1, 30, 10000, 'AVAILABLE'),
+(1, 31, 10000, 'AVAILABLE'),
+(1, 32, 10000, 'AVAILABLE'),
+(1, 33, 10000, 'AVAILABLE'),
+(1, 34, 10000, 'AVAILABLE'),
+(1, 35, 10000, 'AVAILABLE'),
+(1, 36, 10000, 'AVAILABLE'),
+(1, 37, 10000, 'AVAILABLE'),
+(1, 38, 10000, 'AVAILABLE'),
+(1, 39, 10000, 'AVAILABLE'),
+(1, 40, 10000, 'AVAILABLE'),
+(1, 41, 10000, 'AVAILABLE'),
+(1, 42, 10000, 'AVAILABLE'),
+(1, 43, 10000, 'AVAILABLE'),
+(1, 44, 10000, 'AVAILABLE'),
+(1, 45, 10000, 'AVAILABLE'),
+(1, 46, 10000, 'AVAILABLE'),
+(1, 47, 10000, 'AVAILABLE'),
+(1, 48, 10000, 'AVAILABLE'),
+(1, 49, 10000, 'AVAILABLE'),
+(1, 50, 10000, 'AVAILABLE');
