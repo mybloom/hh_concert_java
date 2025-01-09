fb7454f6bc776df89c60209729d3d2004d1f1572
bje <baejungeun11@gmail.com>
2025-01-09 17:43:39 +0900

    QueueOffset 도메인 생성 : 활성 토큰의 max id(auto increment)값 저장

---
+++ src/main/java/kr/hhplus/be/server/domain/queuetoken/domain/QueueOffset.java
@@ -0,0 +1,25 @@
+package kr.hhplus.be.server.domain.queuetoken.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class QueueOffset {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //todo: 여러 인스턴스 동시성 이슈 처리 및 테스트
+    //todo: 스케줄러에서 처리할 수 있도록 변경
+    private Long lastActiveOffset;
+
+
+}
---
+++ src/main/java/kr/hhplus/be/server/domain/queuetoken/infrastructure/QueueOffsetRepositoryImpl.java
@@ -0,0 +1,29 @@
+package kr.hhplus.be.server.domain.queuetoken.infrastructure;
+
+import java.util.Optional;
+import kr.hhplus.be.server.domain.queuetoken.domain.QueueOffset;
+import kr.hhplus.be.server.domain.queuetoken.domain.QueueOffsetRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class QueueOffsetRepositoryImpl implements QueueOffsetRepository {
+
+    private final QueueOffsetJpaRepository queueOffsetJpaRepository;
+
+    @Override
+    public Optional<QueueOffset> findByIdWithLock(long id) {
+        return queueOffsetJpaRepository.findByIdWithLock(id);
+    }
+
+    @Override
+    public QueueOffset save(QueueOffset queueOffset) {
+        return queueOffsetJpaRepository.save(queueOffset);
+    }
+
+    @Override
+    public int updateLastActiveOffset(long id, long lastActiveOffset) {
+        return queueOffsetJpaRepository.updateLastActiveOffset(id, lastActiveOffset);
+    }
+}
