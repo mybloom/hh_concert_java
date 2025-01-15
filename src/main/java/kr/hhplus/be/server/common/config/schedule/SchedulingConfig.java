89dc979fe35ce5b42da1bb5742065f3b94ffe913
bje <baejungeun11@gmail.com>
2025-01-15 10:19:51 +0900

    테스트코드에서 Scheduling 실행되지 않도록하기 위한 SchedulingConfig 설정
    - app.scheduling.enable = false 를 test용 application.yml에 설정하면 되는 옵션

--- src/main/java/kr/hhplus/be/server/common/config/schedule/SchedulingConfig.java
+++ src/main/java/kr/hhplus/be/server/common/config/schedule/SchedulingConfig.java
@@ -1,8 +1,14 @@
 package kr.hhplus.be.server.common.config.schedule;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConditionalOnProperty(
+    value = "app.scheduling.enable",
+    havingValue = "true",
+    matchIfMissing = true
+)
 @Configuration
 @EnableScheduling
 public class SchedulingConfig {
