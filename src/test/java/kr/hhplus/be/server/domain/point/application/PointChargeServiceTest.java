985e522f76b7f4032de0d1a1838710142872fefe
bje <baejungeun11@gmail.com>
2025-01-15 11:07:28 +0900

    [TEST] 포인트 충전 Service 통합테스트 작성

---
+++ src/test/java/kr/hhplus/be/server/domain/point/application/PointChargeServiceTest.java
@@ -0,0 +1,70 @@
+package kr.hhplus.be.server.domain.point.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import kr.hhplus.be.server.domain.point.model.Point;
+import kr.hhplus.be.server.domain.point.repository.PointCommandRepository;
+import kr.hhplus.be.server.domain.point.repository.PointQueryRepository;
+import kr.hhplus.be.server.util.DataCleaner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+//todo: 서비스 통합테스트
+@ActiveProfiles("test")
+@SpringBootTest
+class PointChargeServiceTest {
+
+    @Autowired
+    private PointChargeService pointService;
+
+    @Autowired
+    private PointQueryRepository pointQueryRepository;
+    @Autowired
+    private PointCommandRepository pointCommandRepository;
+
+    @Autowired
+    private DataCleaner dataCleaner;
+
+    @BeforeEach
+    void setUp() {
+        dataCleaner.clean();
+    }
+
+    @DisplayName("충전한 이력이 있는 사용자가 100원을 충전한다.")
+    @Test
+    void chargeForExistingUser() {
+        // given: 이미 포인트가 존재하는 사용자
+        final long userId = 1L;
+        final long pointHeld = 500L;
+        final long pointCharge = 100L;
+
+        Point existingPoint = Point.createInitBalance(userId);
+        existingPoint.charge(pointHeld);
+        pointCommandRepository.save(existingPoint);
+
+        // when: 추가로 충전
+        long newBalance = pointService.charge(userId, pointCharge);
+
+        // then: 600원이 되어야 함
+        assertThat(newBalance).isEqualTo(pointHeld + pointCharge);
+    }
+
+    @DisplayName("충전한 이력이 없는 사용자가 100원을 충전한다.")
+    @Test
+    void chargeForNewUser() {
+        // given
+        final long userId = 1L;
+        final long pointCharge = 100L;
+
+        // when
+        long newBalance = pointService.charge(userId, pointCharge);
+
+        // then
+        assertThat(newBalance).isEqualTo(pointCharge);
+    }
+
+}
