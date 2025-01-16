6bcc6c532826020f015e789a2b4076b3b3d570bc
bje <baejungeun11@gmail.com>
2025-01-17 06:02:12 +0900

    동시성 처리 deadlock 발생

--- src/test/java/kr/hhplus/be/server/domain/point/application/PointServiceConcurrencyTest.java
+++ src/test/java/kr/hhplus/be/server/domain/point/application/PointServiceConcurrencyTest.java
@@ -6,12 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import kr.hhplus.be.server.common.IntegrationTest;
-import kr.hhplus.be.server.domain.point.model.Point;
 import kr.hhplus.be.server.domain.point.repository.PointCommandRepository;
 import kr.hhplus.be.server.domain.point.repository.PointQueryRepository;
 import kr.hhplus.be.server.domain.user.domain.User;
@@ -42,7 +38,7 @@ class PointServiceConcurrencyTest extends IntegrationTest {
     @BeforeEach
     void setUp() {
         user = userRepository.save(new User());
-        pointCommandRepository.save(Point.createInitBalance(user.getId()));
+//        pointCommandRepository.save(Point.createInitBalance(user.getId()));
     }
 
     @DisplayName("한 사용자가 여러 번 충전하는 경우, 충전 금액이 누적되어야 한다.")
@@ -51,37 +47,25 @@ class PointServiceConcurrencyTest extends IntegrationTest {
         //given
         long userId = user.getId();
         long amount = 1000;
-        int chargeCount = 5;
+        int chargeCount = 30;
 
         //비동기 요청 처리 준비
-        ExecutorService executorService = Executors.newFixedThreadPool(20);
         final List<CompletableFuture<Boolean>> tasks = new ArrayList<>(chargeCount);
         final AtomicInteger exceptionCount = new AtomicInteger(0);
 
         //when
         for (int i = 0; i < chargeCount; i++) {
-            tasks.add(CompletableFuture.supplyAsync(() -> { //supplyAsync(), runAsync() : return값 유무
-                    pointChargeService.charge(userId, amount);
-                    return true;
-                }, executorService)
-                .exceptionally(e -> {
-                    exceptionCount.incrementAndGet();
-                    return false;
-                }));
+            tasks.add(CompletableFuture.supplyAsync(() -> {
+                pointChargeService.charge(userId, amount);
+                return true;
+            }).exceptionally(e -> {
+                exceptionCount.incrementAndGet();
+                return false;
+            }));
         }
 
         //then
         CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0])).join();
-        //executor 종료
-        executorService.shutdown();
-        try {
-            if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
-                executorService.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executorService.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
 
         int successCount = 0;
         int failureCount = 0;
