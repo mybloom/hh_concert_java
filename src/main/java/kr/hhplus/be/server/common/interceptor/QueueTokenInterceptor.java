fc91cc403568376b820dc02c710c5015c91998a0
bje <baejungeun11@gmail.com>
2025-01-16 16:42:08 +0900

    [REFACTOR] 대기열 토큰 리팩토링에 따른 QueueTokenInterceptor 변경

--- src/main/java/kr/hhplus/be/server/common/interceptor/QueueTokenInterceptor.java
+++ src/main/java/kr/hhplus/be/server/common/interceptor/QueueTokenInterceptor.java
@@ -1,10 +1,12 @@
 package kr.hhplus.be.server.common.interceptor;
 
+import static kr.hhplus.be.server.common.exception.errorcode.AuthenticationErrorCode.UNAUTHENTICATED_TOKEN;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.hhplus.be.server.common.exception.ErrorResponse;
-import kr.hhplus.be.server.domainold.queuetoken.domain.QueueTokenService;
+import kr.hhplus.be.server.domain.queuetoken.application.QueueTokenValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -15,7 +17,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class QueueTokenInterceptor implements HandlerInterceptor {
 
     private final ObjectMapper objectMapper;
-    private final QueueTokenService queueTokenService;
+    private final QueueTokenValidator queueTokenValidator;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
@@ -29,19 +31,14 @@ public class QueueTokenInterceptor implements HandlerInterceptor {
 
             ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.UNAUTHORIZED,
-                "대기열 토큰을 찾을 수 없습니다. 인증 후 다시 시도해 주세요."
+                UNAUTHENTICATED_TOKEN.getMessage()
             );
             response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
             return false;
         }
 
         //queueToken 검증 로직
-        boolean isValidToken = queueTokenService.isValidToken(queueTokenUuid);
-        if (isValidToken) {
-            return true;
-        }
-
-        return false;
+        return queueTokenValidator.isValidToken(queueTokenUuid);
     }
 
 }
